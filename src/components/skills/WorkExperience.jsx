a9527a4dc83220c1eb968c622712a62858c1a21e
Farkhad <72567229+Farxad-alt@users.noreply.github.com>
2025-10-04 08:43:59 +0300

    Обучение React

---
+++ src/components/skills/WorkExperience.jsx
@@ -0,0 +1,9 @@
+const WorkExporiens = () =>{
+    return(
+        <div>
+            <h1>Work Experience</h1>
+            <p>Work Experience</p>
+        </div>
+    )
+}
+export default WorkExporiens;
---
+++ src/data/skills.js
@@ -0,0 +1,17 @@
+export const skills = [
+  {
+    sill: "HTML", level: "90%"
+  },
+  {
+    sill: "CSS", level: "80%"
+  },
+  {
+    sill: "JavaScript", level: "70%"
+  },
+  {
+    sill: "React", level: "60%"
+  },
+  {
+    sill: "Node.js", level: "50%"
+  }
+]
